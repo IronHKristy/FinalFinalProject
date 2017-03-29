2adc7807c37ecbb057ea56d212fa52bc452926b4
Kristy Horton <kristy@Kristys-MacBook-Air.local>
2017-03-29 19:03:55 -0400

    Fix index path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+const path    = require('path');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render(path.join(___dirname + '/index.html'));
 });
 
 module.exports = router;
